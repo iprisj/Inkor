12de6a201dfe5800712626f823ca5ce28f5745c4
prisj <57462316+iprisj@users.noreply.github.com>
2022-09-02 08:53:19 -0400

    Updated Address

--- DllSrc/Addresses.h
+++ DllSrc/Addresses.h
@@ -2,4 +2,4 @@
 #include <Windows.h>
 
 //805830
-const uintptr_t print_adr = 0x420780; //  0x405230     \xE8\x00\x00\x00\x00\x03\x75\xD0, x????xxx (code pattern)|||(ida pattern) E8 ? ? ? ? 03 75 D0
+const uintptr_t print_adr = 0x424A30; //  0x405230     \xE8\x00\x00\x00\x00\x03\x75\xD0, x????xxx (code pattern)|||(ida pattern) E8 ? ? ? ? 03 75 D0
